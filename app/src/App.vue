9741306668bdac980dd40af7a4c554965e52a53a
unknown <2673693365@qq.com>
2022-05-08 16:02:04 +0800

    feat: el-date-picker el-popover

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,20 +1,65 @@
 <template>
   <div id="app">
-    <canvas id="canvas"></canvas>
+    <el-button
+      ref="btn"
+      @mouseenter.native="handleMouseEnter"
+      @mouseleave.native="handleMouseLeave"
+      >Button
+    </el-button>
   </div>
 </template>
 <script>
-import QRCode from 'qrcode'
+import { DatePicker } from 'element-ui'
+import Vue from 'vue'
 export default {
+  data() {
+    return {}
+  },
   mounted() {
-    var canvas = document.getElementById('canvas')
+    const Ctor = Vue.extend(DatePicker)
 
-    QRCode.toCanvas(canvas, '我爱小宝❤', function (error) {
-      if (error) console.error(error)
-      console.log('success!')
+    const instance = new Ctor({
+      propsData: {
+        valueFormat: 'yyyy-MM-dd',
+        type: 'date',
+        value: '2022-04-07'
+      }
     })
+    instance.$on('input', (val) => {
+      instance.value = val
+    })
+    instance.$mount()
+    instance.$refs.reference.$el = this.$refs.btn.$el
+    instance.mountPicker()
+    this.instance = instance
+    console.log(instance)
+
+    instance.picker.$el.addEventListener('mouseenter', () => {
+      this.handleMouseEnter()
+    })
+    instance.picker.$el.addEventListener('mouseleave', () => {
+      this.handleMouseLeave()
+    })
+  },
+  methods: {
+    handleMouseEnter() {
+      clearTimeout(this.timer)
+      this.instance.showPicker()
+    },
+
+    handleMouseLeave() {
+      this.timer = setTimeout(() => {
+        this.instance.hidePicker()
+      }, 100)
+    }
   }
 }
 </script>
+
+<style scoped>
+.date {
+  display: none;
+}
+</style>
 
 
